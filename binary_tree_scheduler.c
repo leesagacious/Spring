acc4c677ec0b338ed39fef5804a614f4e86628de
leesagacious <sensor1010@163.com>
2017-06-29 21:37:55 +0800

    Update binary_tree_scheduler.c

--- binary_tree_scheduler.c
+++ binary_tree_scheduler.c
@@ -1,8 +1,67 @@
 typedef struct _sched_tree{
     
-    unsigned int prio;
+    unsigned int  prio;
+    unsigned long vir_time;
     
     struct _sched_tree * left;
     struct _sched_tree * right;
     
-}SchedTree;
+}sched_tree;
+
+static struct kmem_cache * sched_tree_cache;
+
+void __init sched_tree_init_cache(void)
+{
+	sched_tree_cache = kmem_cache_create("sched_tree_cache",sizeof(sched_tree),0, 
+                                         (SLAB_RECLAIM_ACCOUNT|SLAB_MEM_SPREAD),NULL);
+    if (!sched_tree_cache){
+		return -ENOMEM;
+    }
+    
+    return 0;
+}
+
+int relatively(unsigned int original_node_vir_time,unsinged int new_node_prio)
+{
+    return original_node_vir_time > new_node_prio;
+}  
+
+void insertSchedTree(sched_tree ** source,int ( * relatively )(unsigned int,unsigned int))
+{
+    sched_tree * tree_node = kmem_cache_alloc(sched_tree_cache,GFP_KERNEL);
+    if (!tree_ode){
+        pr_err("kmem_cache_alloc has failure\n");
+        return;
+    }
+    
+    tree_node->left  = NULL;
+    tree_node->right = NULL;
+    
+    if (*source == NULL){
+        *source = tree_node;
+        return;
+    }
+    
+    while (1)
+    {
+            if (relatively(*source->prio,prio) > 0)   //比较.
+            {
+                if ((*source)->left){            //如果左子节点非空
+                    *source = (*root)->left;     //查找下一个左子节点
+                }else {
+                    (*source)->left = node;     // 添加 
+                    break;
+                }
+            }
+            else 
+            {
+                if ((*source)->right)
+                {
+                    *source = (*source)->right;
+                }else {
+                    (*source)->right = node;
+                    break;
+                }
+            }    
+    } 
+}
